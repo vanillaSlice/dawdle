c1de01e4d0c5409f408576fc32606b78e5efc3cb
Mike Lowe <mikelowedev@gmail.com>
2019-05-19 13:56:58 +0100

    Separate bundles

--- dawdle/assets.py
+++ dawdle/assets.py
@@ -5,14 +5,6 @@ Exports asset bundles to be used in the UI.
 from flask_assets import Bundle
 
 bundles = {
-    'all_js': Bundle(
-        '**/*.js',
-        filters='jsmin',
-        output='build/bundle.min.js'
-    ),
-    'all_css': Bundle(
-        '**/*.css',
-        filters='cssmin',
-        output='build/bundle.min.css'
-    )
+    'app_js': Bundle('scripts/app.js', filters='jsmin', output='build/app.min.js'),
+    'app_css': Bundle('styles/app.css', filters='cssmin', output='build/app.min.css'),
 }
