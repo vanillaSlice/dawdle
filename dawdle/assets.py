6ffdb4f3090cf9ed61a673d64071333c470bdf0d
Mike Lowe <mikelowedev@gmail.com>
2019-05-19 16:52:50 +0100

    Tidy template structure

--- dawdle/assets.py
+++ dawdle/assets.py
@@ -5,6 +5,14 @@ Exports asset bundles to be used in the UI.
 from flask_assets import Bundle
 
 bundles = {
-    'app_js': Bundle('scripts/app.js', filters='jsmin', output='build/app.min.js'),
-    'app_css': Bundle('styles/app.css', filters='cssmin', output='build/app.min.css'),
+    'app_js': Bundle(
+        'scripts/app.js', 'styles/shared/*.js',
+        filters='jsmin',
+        output='build/app.min.js',
+    ),
+    'app_css': Bundle(
+        'styles/app.css', 'styles/shared/*.css',
+        filters='cssmin',
+        output='build/app.min.css',
+    ),
 }
