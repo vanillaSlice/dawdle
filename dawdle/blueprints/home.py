9e3ff355ca74b87921bd7df3ae23b800e867c9a1
Mike Lowe <mikelowedev@gmail.com>
2019-05-26 21:52:52 +0100

    Redirect to boards page if logged in

--- dawdle/blueprints/home.py
+++ dawdle/blueprints/home.py
@@ -2,7 +2,8 @@
 Exports Home routes.
 """
 
-from flask import Blueprint, render_template
+from flask import Blueprint, redirect, render_template, url_for
+from flask_login import current_user
 
 home = Blueprint('home', __name__, url_prefix='/')
 
@@ -12,4 +13,9 @@ def index():
     Index route.
     """
 
+    # redirect to user's boards page if already logged in
+    if current_user.is_authenticated:
+        return redirect(url_for('user.boards', user_id=str(current_user.id)))
+
+    # otherwise render home page
     return render_template('home/index.html')
--- tests/test_home.py
+++ tests/test_home.py
@@ -6,6 +6,11 @@ class TestHome(TestBase):
         response = self.client.get('/this/page/does/not/exist')
         assert response.status_code == 404
 
-    def test_index(self):
+    def test_index_not_authenticated(self):
+        self.logout()
         response = self.client.get('/')
         assert response.status_code == 200
+
+    def test_index_authenticated(self):
+        response = self.client.get('/')
+        assert response.status_code == 302
