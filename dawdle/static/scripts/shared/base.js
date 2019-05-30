1110524b9cbf3bd7946e8444986a1a5e5a04f5e6
Mike Lowe <mikelowedev@gmail.com>
2019-05-30 21:54:37 +0100

    Better front end validation display

--- dawdle/static/scripts/shared/base.js
+++ dawdle/static/scripts/shared/base.js
@@ -1,10 +1,23 @@
 document.addEventListener('DOMContentLoaded', function() {
 
   // hides notifications when 'delete' button clicked
-  (document.querySelectorAll('.notification .delete') || []).forEach(function(el) {
+  document.querySelectorAll('.notification .delete').forEach(function(el) {
     notification = el.parentNode;
     el.addEventListener('click', function() {
       notification.parentNode.removeChild(notification);
+    });
+  });
+
+  // toggles 'is-danger' class on input when invalid
+  document.querySelectorAll('input').forEach(function(el) {
+    el.addEventListener('invalid', function() {
+      el.classList.add('is-danger');
+    });
+
+    el.addEventListener('input', function() {
+      if (el.validity.valid) {
+        el.classList.remove('is-danger');
+      }
     });
   });
 });
