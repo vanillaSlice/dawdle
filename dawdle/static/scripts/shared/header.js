8c9ab6ed6ecc9cb76703c9108d987774cd3b34c1
Mike Lowe <mikelowedev@gmail.com>
2019-05-30 21:30:56 +0100

    Static files tidy

--- dawdle/static/scripts/shared/header.js
+++ dawdle/static/scripts/shared/header.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
 
-  // Get all "navbar-burger" elements
+  // Get all 'navbar-burger' elements
   var navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
 
   // Check if there are any navbar burgers
@@ -10,10 +10,10 @@ document.addEventListener('DOMContentLoaded', function() {
     navbarBurgers.forEach(function(el) {
       el.addEventListener('click', function() {
 
-        // Get the target from the "data-target" attribute
+        // Get the target from the 'data-target' attribute
         var target = document.getElementById(el.dataset.target);
 
-        // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+        // Toggle the 'is-active' class on both the 'navbar-burger' and the 'navbar-menu'
         el.classList.toggle('is-active');
         target.classList.toggle('is-active');
       });
