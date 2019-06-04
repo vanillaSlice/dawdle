58381b6fc130454fca0bbc11cecf842d7a89bb0c
Mike Lowe <mikelowedev@gmail.com>
2019-06-04 20:33:54 +0100

    Use js classes instead of style classes

--- dawdle/static/scripts/shared/base.js
+++ dawdle/static/scripts/shared/base.js
@@ -1,33 +1,33 @@
 $(document).ready(function() {
 
-  // hides notifications when 'delete' button clicked
-  $('.notification .delete').click(function() {
+  // hides notifications when clicked
+  $('.js-notification-close').click(function() {
     $(this).parent().remove();
   });
 
-  // adds error class to field element when invalid
-  $('input').on('invalid', function() {
+  // adds error class to form field when invalid
+  $('.js-form-field').on('invalid', function() {
     toggleFieldElementErrorClass($(this), true);
   });
 
-  // removes error class from field element when valid
-  $('input').on('input', function() {
+  // removes error class from form field when valid
+  $('.js-form-field').on('input', function() {
     toggleFieldElementErrorClass($(this), false);
   });
 
   function toggleFieldElementErrorClass(fieldElement, state) {
     if (state || fieldElement[0].validity.valid) {
       fieldElement.toggleClass('is-danger', state);
-      $('#' + fieldElement.attr('id') + '-help').toggleClass('is-danger', state);
+      $(fieldElement.data('help')).toggleClass('is-danger', state);
     }
   }
 
-  // add active class to target modal when clicked
+  // displays modal when clicked
   $('.js-modal-open').click(function() {
     $($(this).data('target')).addClass('is-active');
   });
 
-  // remove active class from target modal when clicked
+  // hides modal when clicked
   $('.js-modal-close').click(function() {
     $($(this).data('target')).removeClass('is-active');
   });
--- dawdle/static/scripts/shared/header.js
+++ dawdle/static/scripts/shared/header.js
@@ -1,9 +1,10 @@
 $(document).ready(function() {
 
-  // displays menu when 'navbar-burger' clicked
-  $('.navbar-burger').click(function() {
+  // displays navbar menu when clicked
+  $('.js-navbar-burger').click(function() {
     var navbarBurgerElement = $(this);
     navbarBurgerElement.toggleClass('is-active');
+    navbarBurgerElement.attr('aria-expanded', navbarBurgerElement.attr('aria-expanded') === 'false');
     $(navbarBurgerElement.data('target')).toggleClass('is-active');
   });
 });
