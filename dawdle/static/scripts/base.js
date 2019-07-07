9799c5f166300c745e63928f2ddbc93a4d29c859
Mike Lowe <mikelowedev@gmail.com>
2019-07-07 21:29:36 +0100

    Tidy JS exported functions

--- dawdle/static/scripts/base.js
+++ dawdle/static/scripts/base.js
@@ -4,78 +4,74 @@
    * Navbar
    */
 
-  function handleNavbarBurgerClick() {
-    var navbarBurgerElement = $(this);
-    var navbarMenuElement = $(navbarBurgerElement.data('target'));
+  function toggleNavbar(navbarElement) {
+    var navbarBurgerElement = navbarElement.find('.js-navbar-burger');
+    var navbarMenuElement = navbarElement.find('.js-navbar-menu');
     var isActive = navbarBurgerElement.hasClass('is-active');
     navbarBurgerElement.toggleClass('is-active', !isActive);
     navbarBurgerElement.attr('aria-expanded', !isActive);
     navbarMenuElement.toggleClass('is-active', !isActive);
   }
 
-  $('.js-navbar-burger').click(handleNavbarBurgerClick);
+  $('.js-navbar-burger').click(function() {
+    toggleNavbar($(this).parents('.js-navbar'));
+  });
 
   /*
    * Notifications
    */
 
-  function handleNotificationCloseClick() {
+  $('.js-notification-close').click(function() {
     $(this).parent().remove();
-  }
-
-  $('.js-notification-close').click(handleNotificationCloseClick);
+  });
 
   /*
    * Forms
    */
 
-  function handleFormSubmit() {
-    $(this).find('.js-submit').attr('disabled', true);
-  }
-
-  $('.js-form').submit(handleFormSubmit);
-
-  function handleFieldInput() {
-    toggleFieldElementErrorClass($(this), false);
-  }
-
-  function handleFieldInvalid() {
-    toggleFieldElementErrorClass($(this), true);
-  }
+  $('.js-form').submit(function() {
+    $(this).find('.js-submit').prop('disabled', true);
+  });
 
   function toggleFieldElementErrorClass(fieldElement, state) {
     if (!state && !fieldElement[0].validity.valid) {
       return;
     }
 
+    var formFieldContainerElement = fieldElement.parents('.js-form-field-container');
+    var helpElement = formFieldContainerElement.find('.js-help');
+    var passwordMaskElement = formFieldContainerElement.find('.js-password-mask');
     fieldElement.toggleClass('is-danger', state);
-
-    var helpElement = $(fieldElement.data('help'));
     helpElement.toggleClass('is-danger', state);
-
-    var passwordMaskElement = $(fieldElement.data('password-mask'));
     passwordMaskElement.toggleClass('is-danger', state);
   }
 
-  $('.js-form-field').on('input', handleFieldInput);
-  $('.js-form-field').on('invalid', handleFieldInvalid);
+  $('.js-form-field').on('input', function() {
+    toggleFieldElementErrorClass($(this), false);
+  });
 
-  function handlePasswordMaskClick() {
-    var buttonElement = $(this);
-    var fieldElement = $(buttonElement.data('field'));
-    var iconElement = buttonElement.find('.js-password-mask-icon');
-    if (fieldElement.attr('type') === 'password') {
-      fieldElement.attr('type', 'text');
+  $('.js-form-field').on('invalid', function() {
+    toggleFieldElementErrorClass($(this), true);
+  });
+
+  function togglePasswordMask(buttonElement) {
+    var formFieldContainerElement = buttonElement.parents('.js-form-field-container');
+    var fieldElement = formFieldContainerElement.find('.js-form-field');
+    var iconElement = formFieldContainerElement.find('.js-password-mask-icon');
+    if (fieldElement.prop('type') === 'password') {
+      fieldElement.prop('type', 'text');
       iconElement.removeClass('fa-eye');
       iconElement.addClass('fa-eye-slash');
     } else {
-      fieldElement.attr('type', 'password');
+      fieldElement.prop('type', 'password');
       iconElement.removeClass('fa-eye-slash');
       iconElement.addClass('fa-eye');
     }
   }
 
-  $('.js-password-mask').click(handlePasswordMaskClick);
+  $('.js-password-mask').click(function() {
+    togglePasswordMask($(this));
+  });
 
   /*
    * Truncation
@@ -93,34 +89,28 @@
    * Modals
    */
 
-  function toggleModal(element) {
-    element.toggleClass('is-active');
-    $('html, body').toggleClass('is-clipped');
+  function toggleModal(modalElement) {
+    modalElement.toggleClass('is-active');
+    $('html, body, main').toggleClass('is-clipped');
   }
 
-  function handleModalTriggerClick() {
+  $('.js-modal-trigger').click(function() {
     var triggerElement = $(this);
     var target = triggerElement.data('target');
     var modalElement = target ? $(target) : triggerElement.parents('.js-modal');
     triggerElement.blur();
     toggleModal(modalElement);
-  }
-
-  $('.js-modal-trigger').click(handleModalTriggerClick);
+  });
 
   /*
    * Export functions
    */
 
   window.dawdle = {
-    handleFieldInput: handleFieldInput,
-    handleFieldInvalid: handleFieldInvalid,
-    handleFormSubmit: handleFormSubmit,
-    handleModalTriggerClick: handleModalTriggerClick,
-    handleNavbarBurgerClick: handleNavbarBurgerClick,
-    handleNotificationCloseClick: handleNotificationCloseClick,
-    handlePasswordMaskClick: handlePasswordMaskClick,
+    toggleFieldElementErrorClass: toggleFieldElementErrorClass,
     toggleModal: toggleModal,
+    toggleNavbar: toggleNavbar,
+    togglePasswordMask: togglePasswordMask,
     truncateText: truncateText,
   };
 })();
