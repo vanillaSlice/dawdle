64fce82e809f76b2effb299322de2edd432cbaa8
Mike Lowe <mikelowedev@gmail.com>
2020-07-06 15:34:30 +0100

    Add model test (#413)

---
+++ api/tests/components/user/test_models.py
@@ -0,0 +1,21 @@
+# pylint: disable=no-self-use
+
+from datetime import datetime
+
+from bson.objectid import ObjectId
+
+from dawdle.components.user.models import User
+
+
+class TestModels:
+
+    def test_User_defaults(self):
+        user = User()
+        assert not user.active
+        assert isinstance(user.auth_id, ObjectId)
+        assert isinstance(user.created, datetime)
+        assert not user.email
+        assert not user.initials
+        assert isinstance(user.last_updated, datetime)
+        assert not user.name
+        assert not user.password
