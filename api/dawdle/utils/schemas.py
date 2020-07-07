4f51a91143743c3be9dee04fe8ec8f78cb78ac5a
Mike Lowe <mikelowedev@gmail.com>
2020-07-07 15:13:13 +0100

    Trim string bug (#427)

--- api/dawdle/utils/schemas.py
+++ api/dawdle/utils/schemas.py
@@ -2,8 +2,9 @@ from dawdle.utils import remove_extra_whitespace
 
 
 def trim_string(in_data, key):
-    if key in in_data:
-        trimmed = remove_extra_whitespace(in_data[key])
+    value = in_data.get(key)
+    if isinstance(value, str):
+        trimmed = remove_extra_whitespace(value)
         if trimmed:
             in_data[key] = trimmed
         else:
